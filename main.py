21a5ba962a7d990e29fe3d5be944153f8dcaf2e7
dalamity <mrslime43@gmail.com>
2025-10-30 16:35:48 -0700

    Add some very very basic auth

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
+import stat
+from typing import Annotated
 import state
+import os
 from datetime import datetime, timedelta
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException
+from fastapi.security import OAuth2PasswordBearer
 from pydantic import BaseModel
 import ipaddress 
 
 currentState = state.MinecraftState()
+secretKey = os.environ.get("SECRET_KEY", "1234")
 
 class InfoPacket(BaseModel):
     ip: str
 
 app = FastAPI()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 @app.get("/")
 def read_root():
@@ -22,7 +28,11 @@ def read_root():
     }
 
 @app.patch("/update/")
-async def update_info(newInfo: InfoPacket):
+async def update_info(token: Annotated[str, Depends(oauth2_scheme)], newInfo: InfoPacket):
+    if token != secretKey:
+        raise HTTPException(
+            status_code=401
+        )
     def check_info():
         assert ipaddress.ip_address(newInfo.ip)
 
