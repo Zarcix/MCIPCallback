03000b718b48ba08e14aef26e36636d4a51ab612
dalamity <mrslime43@gmail.com>
2025-10-30 16:15:13 -0700

    Add some more stuff

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import state
 from datetime import datetime, timedelta
 from fastapi import FastAPI
-from pydantic import BaseModel  
+from pydantic import BaseModel
+import ipaddress 
 
 currentState = state.MinecraftState()
 
@@ -16,10 +17,15 @@ def read_root():
     mcHealth = currentState.lastUpdated != None and (datetime.now() - currentState.lastUpdated) <= timedelta(minutes=5)
     return {
         "ip": mcIP,
-        "healthy": mcHealth
+        "healthy": mcHealth,
+        "lastUpdated": currentState.lastUpdated.strftime("%d %B, %Y %H:%M:%S") if currentState.lastUpdated != None else None
     }
 
 @app.patch("/update/")
 async def update_info(newInfo: InfoPacket):
+    def check_info():
+        assert ipaddress.ip_address(newInfo.ip)
+
+    check_info()
     currentState.currentIP = newInfo.ip
     currentState.lastUpdated = datetime.now()
